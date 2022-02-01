80ac12212f0cd8d04aada2bf454039ca224350ab
Integralexei <integralexei@gmail.com>
2022-02-01 19:12:22 +0300

    some modifications, os.environ

--- app/hello.py
+++ app/hello.py
@@ -1,16 +1,21 @@
+import dotenv
+import os
+from dotenv import load_dotenv 
 from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+load_dotenv()
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:7788@localhost/pairs_flask'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql+psycopg2://postgres:{os.environ.get("POSTGRES_PASS")}@localhost/pairs_flask'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 @app.route('/')
 def index():
-    return render_template('index.html', name='Harry')
+    return render_template('index.html', name='Marry')
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -33,13 +38,4 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
-if __name__ == '__main__':
-    app.run(debug=True)
 
-# @app.route('/')
-# def index():
-#     return '<h1>Чёкаво!!!</h1>'
-
-# @app.route('/user/<name>')
-# def user(name):
-#     return '<h1>Hello, {}!</h1>'.format(name)
