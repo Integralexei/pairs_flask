ddb2ff7056664adea88e12af48a69257bffb4257
Integralexei <integralexei@gmail.com>
2022-02-01 15:43:15 +0300

    before adding db

--- app/hello.py
+++ app/hello.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Чёкаво!!!</h1>'
+    return render_template('index.html', name='Harry')
 
-@app.route('/user/<name>')
-def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+# @app.route('/')
+# def index():
+#     return '<h1>Чёкаво!!!</h1>'
+
+# @app.route('/user/<name>')
+# def user(name):
+#     return '<h1>Hello, {}!</h1>'.format(name)
