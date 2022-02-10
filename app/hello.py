61c3a59176f33f2d0d9bab85d17ae3cfa2e1033e
Integralexei <integralexei@gmail.com>
2022-02-10 14:39:13 +0300

    add streaming and charting on main page

--- app/hello.py
+++ app/hello.py
@@ -1,11 +1,16 @@
 import dotenv
 import os
 from dotenv import load_dotenv 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, Response
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import json
+import time
+from datetime import datetime
+import random
 
 load_dotenv()
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql+psycopg2://postgres:{os.environ.get("POSTGRES_PASS")}@localhost/pairs_flask'
@@ -13,32 +18,45 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+random.seed()
+
 @app.route('/')
 def index():
-    # return render_template('index.html', name='Marry')
     return render_template('index.html')
 
-class Role(db.Model):
-    __tablename__ = 'roles'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True)
+@app.route('/chart-data')
+def chart_data():
+    def generate_random_data():
+        while True:
+            json_data = json.dumps(
+                {'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 'value': random.random() * 100})
+            yield f"data:{json_data}\n\n"
+            time.sleep(1)
 
-    users = db.relationship('User', backref='role')
+    return Response(generate_random_data(), mimetype='text/event-stream')
+
+# class Role(db.Model):
+#     __tablename__ = 'roles'
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(64), unique=True)
+
+#     users = db.relationship('User', backref='role')
 
 
-    def __repr__(self):
-        return '<Role %r>' % self.name
+#     def __repr__(self):
+#         return '<Role %r>' % self.name
 
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True, index=True)
+# class User(db.Model):
+#     __tablename__ = 'users'
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(64), unique=True, index=True)
 
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+#     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+#     def __repr__(self):
+#         return '<User %r>' % self.username
 
-# if __main__ == __name__
+if __name__ == "__main__":
+    app.run(debug=True, threaded = True)
 
 
