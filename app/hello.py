8b1d2ffdbc35ac6c53e3702ef0a9a3a9430cc736
unknown <yesterday69>
2022-02-13 00:55:33 +0300

    added new file boot.html with new design page with bootstrap. all works

--- app/hello.py
+++ app/hello.py
@@ -11,8 +11,8 @@ import requests
 
 
 load_dotenv()
-env_path = r'C:\py_proj\pairs_flask\.env'
-load_dotenv(dotenv_path=env_path)
+# env_path = r'C:\py_proj\pairs_flask\.env'
+# load_dotenv(dotenv_path=env_path)
 
 
 
@@ -22,15 +22,15 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-auth = os.environ.get("AUTH_EXANTE_DEMO")
-headers={'Accept':'application/x-json-stream'}
-r = requests.get('https://api-demo.exante.eu/md/3.0/feed/trades/ETH.USD',stream=True, headers=headers, auth=auth)
-
 random.seed()
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/boot')
+def boot():
+    return render_template('boot.html')
 
 @app.route('/chart-data')
 def chart_data():
@@ -39,23 +39,20 @@ def chart_data():
             json_data = json.dumps(
                 {'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 'value': random.random() * 100})
             yield f"data:{json_data}\n\n"
-            time.sleep(1)
+            # time.sleep(1)
 
     return Response(generate_random_data(), mimetype='text/event-stream')
 
 
 def get_line_stream():
-        print('1111111')
         api_key = os.environ.get('API_KEY_EXANTE_DEMO')
         token = os.environ.get('TOKEN_EXANTE_DEMO')
         headers={'Accept':'application/x-json-stream'}
-        r = requests.get('https://api-demo.exante.eu/md/3.0/feed/trades/ETH.USD',stream=True, headers=headers, auth=api_key+','+token)
-        print(r, 'request')
+        r = requests.get('https://api-demo.exante.eu/md/3.0/feed/trades/ETH.USD',stream=True, headers=headers, auth=(api_key,token))
+        print(r)
         for chunk in r.iter_lines(chunk_size=1):
             if chunk:
                 yield chunk.decode('utf8')
-                print('111')
-                # time.sleep(1)
 
 streamExante = get_line_stream() 
 
@@ -64,21 +61,19 @@ def chart_data1():
     def generate_data():
         date_obj = datetime.now()
         for i in streamExante:
-            print('22222')
             i = json.loads(i)
             if "price" in i: 
                 date_obj = datetime.fromtimestamp(i['timestamp']//1000)
                 i['timestamp'] = str(datetime.fromtimestamp(i['timestamp']//1000))
-                json_data = json.dumps({'time': i['timestamp'], 'value': float(i['price'])})
+                json_data = json.dumps({'time': i['timestamp'][-8:], 'value': float(i['price'])})
                 print(json_data, 'jsondata')
                 yield f"data:{json_data}\n\n"
-                # time.sleep(1)
             else:   
                 continue
 
     return Response(generate_data(), mimetype='text/event-stream')
 
-# class Role(db.Model):
+# class Role(db.Model): .
 #     __tablename__ = 'roles'
 #     id = db.Column(db.Integer, primary_key=True)
 #     name = db.Column(db.String(64), unique=True)
