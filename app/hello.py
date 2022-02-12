8996e1eae9eb214693327cca8bd5f08959d0e7b7
Integralexei <integralexei@gmail.com>
2022-02-12 17:00:39 +0300

    livechart works but environget doesnt need to fix auth

--- app/hello.py
+++ app/hello.py
@@ -1,6 +1,5 @@
-import dotenv
+from dotenv import load_dotenv
 import os
-from dotenv import load_dotenv 
 from flask import Flask, request, render_template, Response
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -8,8 +7,13 @@ import json
 import time
 from datetime import datetime
 import random
+import requests
+
 
 load_dotenv()
+env_path = r'C:\py_proj\pairs_flask\.env'
+load_dotenv(dotenv_path=env_path)
+
 
 
 app = Flask(__name__)
@@ -17,6 +21,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql+psycopg2://postgres:{os.env
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+auth = os.environ.get("AUTH_EXANTE_DEMO")
+headers={'Accept':'application/x-json-stream'}
+r = requests.get('https://api-demo.exante.eu/md/3.0/feed/trades/ETH.USD',stream=True, headers=headers, auth=auth)
 
 random.seed()
 
@@ -34,6 +42,41 @@ def chart_data():
             time.sleep(1)
 
     return Response(generate_random_data(), mimetype='text/event-stream')
+
+
+def get_line_stream():
+        print('1111111')
+        api_key = os.environ.get('API_KEY_EXANTE_DEMO')
+        token = os.environ.get('TOKEN_EXANTE_DEMO')
+        headers={'Accept':'application/x-json-stream'}
+        r = requests.get('https://api-demo.exante.eu/md/3.0/feed/trades/ETH.USD',stream=True, headers=headers, auth=api_key+','+token)
+        print(r, 'request')
+        for chunk in r.iter_lines(chunk_size=1):
+            if chunk:
+                yield chunk.decode('utf8')
+                print('111')
+                # time.sleep(1)
+
+streamExante = get_line_stream() 
+
+@app.route('/chart-data1')
+def chart_data1():
+    def generate_data():
+        date_obj = datetime.now()
+        for i in streamExante:
+            print('22222')
+            i = json.loads(i)
+            if "price" in i: 
+                date_obj = datetime.fromtimestamp(i['timestamp']//1000)
+                i['timestamp'] = str(datetime.fromtimestamp(i['timestamp']//1000))
+                json_data = json.dumps({'time': i['timestamp'], 'value': float(i['price'])})
+                print(json_data, 'jsondata')
+                yield f"data:{json_data}\n\n"
+                # time.sleep(1)
+            else:   
+                continue
+
+    return Response(generate_data(), mimetype='text/event-stream')
 
 # class Role(db.Model):
 #     __tablename__ = 'roles'
