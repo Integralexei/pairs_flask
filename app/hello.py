8448f5fabe538dc7dff708a6d19198dc61c71d11
Integralexei <integralexei@gmail.com>
2022-02-15 13:51:06 +0300

    fix generator exec already

--- app/hello.py
+++ app/hello.py
@@ -47,23 +47,19 @@ def get_line_stream(ticker):
         r = requests.get(f'https://api-demo.exante.eu/md/3.0/feed/trades/{ticker}',stream=True, headers=headers, auth=(api_key,token))
         print(f'{r}___{ticker}')
         for chunk in r.iter_lines(chunk_size=1):
+            print()
             if chunk:
                 yield chunk.decode('utf8')
-                # yield chunk
 
-streamExante1 = get_line_stream('BTC.USD') 
-streamExante2 = get_line_stream('ETH.USD') 
-# streamExante_second_paper = get_line_stream('ETH.USD') 
 
 @app.route('/chart-data1')
 def chart_data1():
     def generate_data():
         date_obj = datetime.now()
-        for i in zip(streamExante1, streamExante2) :
+        for i in zip(get_line_stream('SBER.MICEX'), get_line_stream('SBERP.MICEX')) :
             i = [json.loads(x) for x in i] # convert str to type json
             if "price" in i[0] and "price" in i[1]: 
                 date_obj = [datetime.fromtimestamp(x['timestamp']//1000) for x in i]
-                # if i[0]['timestamp'] == i[1]['timestamp']:
                 i[0]['timestamp'] = str(date_obj[0])[-8:]
                 i[1]['timestamp'] = str(date_obj[1])[-8:]
                 print(f'{i}\n\n')
@@ -71,33 +67,11 @@ def chart_data1():
                 json_data2 = i[1]
                 json_data = json.dumps({'time1': json_data1['timestamp'], 'value1': float(json_data1['price']),
                                         'time2': json_data2['timestamp'], 'value2': float(json_data2['price'])})
-                # print(f'{json_data1}\n{json_data2}')
                 yield f"data:{json_data}\n\n"
-      
-                # else:
-                #     continue
             else:   
                 continue
 
     return Response(generate_data(), mimetype='text/event-stream')
-
-# @app.route('/chart-data1')
-# def chart_data1():
-#     def generate_data():
-#         date_obj = datetime.now()
-#         for i in zip(streamExante1,streamExante2):
-#             print(i)
-#             i = json.loads(i)
-#             if "price" in i: 
-#                 date_obj = datetime.fromtimestamp(i['timestamp']//1000)
-#                 i['timestamp'] = str(datetime.fromtimestamp(i['timestamp']//1000))
-#                 json_data = json.dumps({'time': i['timestamp'][-8:], 'value': float(i['price'])})
-#                 print(json_data, 'jsondata')
-#                 yield f"data:{json_data}\n\n"
-#             else:   
-#                 continue
-
-#     return Response(generate_data(), mimetype='text/event-stream')
 
 
 # class Role(db.Model): .
