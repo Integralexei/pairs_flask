f6811876a928e1d3541c3c7c04dd2937ef42c7c2
Integralexei <integralexei@gmail.com>
2022-02-23 20:46:19 +0300

    insert symbols into db and delete it in tryings.ipynb

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from datetime import datetime
 class Symbol(db.Model):
     __tablename__ = 'symbols'
     id = db.Column(db.Integer, primary_key=True)
-    symbol_name = db.Column(db.String(20), nullable=False)
+    symbol_name = db.Column(db.String(16), nullable=False)
 
     prices = db.relationship('Price', backref='symbols')
     
@@ -14,8 +14,8 @@ class Price(db.Model):
     __tablename__ = 'prices'
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.DateTime, index=True)
-    symbol_name = db.Column(db.String(50), nullable=False)
-    price = db.Column(db.Numeric(20, 4), nullable=False)
+    symbol_name = db.Column(db.String(64), nullable=False)
+    price = db.Column(db.Numeric(16, 4), nullable=False)
 
     symbol_id = db.Column(db.Integer(), db.ForeignKey('symbols.id'), nullable=False)
     
@@ -24,36 +24,3 @@ class Price(db.Model):
 
 #    https://smart-lab.ru/blog/422980.php db schema
 
-# class Role(db.Model): 
-#     __tablename__ = 'roles'
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(64), unique=True)
-
-#     users = db.relationship('User', backref='role')
-
-#     def __repr__(self):
-#         return '<Role %r>' % self.name
-
-# class User(db.Model):
-#     __tablename__ = 'users'
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(64), unique=True, index=True)
-
-#     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-
-#     posts = db.relationship('Post', backref='users')
-
-#     def __repr__(self):
-#         return '<User %r>' % self.username
-
-# class Post(db.Model):
-#     __tablename__ = 'posts'
-#     id = db.Column(db.Integer, primary_key=True)
-#     title = db.Column(db.String(255), nullable=False)
-#     content = db.Column(db.Text(), nullable=False)
-#     created_on = db.Column(db.DateTime(), default=datetime.utcnow)
-
-#     user_id = db.Column(db.Integer(), db.ForeignKey('users.id'))
-
-#     def __repr__(self):
-#         return "<{}:{}>".format(self.id,  self.title[:10])
